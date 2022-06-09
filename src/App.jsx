779a721f8276791340d8ca269151ec7c65bda732
Ritesh Sapata <riteshsapata@gmail.com>
2022-06-09 12:32:24 +0530

    custom blocks created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import BlocklyEditor from './Components/BlocklyEditor/BlocklyEditor';
+import Editor from './Components/Editor/Editor';
 import MenuBar from './Components/MenuBar/MenuBar';
 
 function App() {
     return (
         <div className="">
             <MenuBar />
-            <BlocklyEditor />
+            {/* <BlocklyEditor /> */}
+            <Editor />
         </div>
     );
 }
--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import BlocklyEditor from '../Components/BlocklyEditor/BlocklyEditor';
 import Canvas from '../Components/Canvas/Canvas';
+import Editor from '../Components/Editor/Editor';
 
 const Game = () => {
     return (
         <div>
-            <BlocklyEditor />
+            <Editor />
             <Canvas />
         </div>
     );
