004b3c11d69067c4ffaea8b35d61069d82ad50c5
Ritesh Sapata <riteshsapata@gmail.com>
2022-06-29 16:53:37 +0530

    initialised the production app

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className='App'>
+            <h1 className='text-4xl'>Wiingy Studio</h1>
+        </div>
+    );
+}
+
+export default App;
